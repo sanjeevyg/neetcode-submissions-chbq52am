86ab27b42e13a1218a30a6890b160b9980c3cf45
Sanjeev Yogi <yogi.sjv@gmail.com>
2026-04-03 09:20:48 +0300

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let j = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        console.log(nums);
+        return j;
+    }
+}
