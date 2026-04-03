a7e8bc7afde9479aea5d62e145c14222c1a02799
Sanjeev Yogi <yogi.sjv@gmail.com>
2026-04-03 14:10:37 +0300

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+
+        let ans = new Array(n).fill(1);
+
+        let prefix = 1;
+        for(let i = 0; i < n; i++) {
+            ans[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+
+        let suffix = 1;
+        for(let i = n - 1; i >= 0; i--) {
+            ans[i] = ans[i] * suffix;
+            suffix = nums[i] * suffix
+        }
+        return ans;
+    }
+}
