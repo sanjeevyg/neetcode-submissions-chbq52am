ee6832e6ba71553f5c3ff56a8cd5e6700f395e0d
Sanjeev Yogi <yogi.sjv@gmail.com>
2026-08-06 18:52:22 +0200

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //use stringBuidler to append string
+        //use java isLetterOrDigit method to skip non-alphanumeric characters
+        //use reverse() method to see if they are palindrome
+
+        char[] str = s.toCharArray();
+
+        StringBuilder sb = new StringBuilder();
+        for (char c : str) {
+            if(Character.isLetterOrDigit(c)) {
+                sb.append(c);
+            }
+        }
+        // System.out.println(sb.reverse().toString());
+        if (sb.toString().toLowerCase().equals(sb.reverse().toString().toLowerCase())) {
+            return true;
+        }
+
+        return false;
+    }
+}
