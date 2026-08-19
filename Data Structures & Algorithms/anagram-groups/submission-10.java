aeda62fcd3ca2fd8b3942ba1c91a1f35bec11d80
Sanjeev Yogi <yogi.sjv@gmail.com>
2026-08-19 21:52:38 +0200

    Add: anagram-groups - submission-10

---
+++ Data Structures & Algorithms/anagram-groups/submission-10.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        //use map to devise key for each string
+        //use common key to list anagrams
+        //return values
+
+        Map<String, List<String>> tracker = new HashMap<>();
+
+        for (String str : strs) {
+            StringBuilder sb = new StringBuilder();
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            for(char s : arr) {
+                sb.append('#').append(s);
+            }
+            String key = sb.toString();
+            tracker.putIfAbsent(key, new ArrayList<>());
+            tracker.get(key).add(str);
+        }
+        // System.out.println(tracker.values());
+
+
+        return new ArrayList<>(tracker.values());
+        // return new ArrayList<>(new ArrayList<>());
+
+
+    }
+}
