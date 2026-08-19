16e55626105ae4c362ba9c58e31ad8ad943c2a75
Sanjeev Yogi <yogi.sjv@gmail.com>
2026-08-19 21:33:02 +0200

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> tracker = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++) {
+            int compliment = target - nums[i];
+            if(tracker.containsKey(compliment)) {
+                return new int[] {tracker.get(compliment), i};
+            }
+            tracker.put(nums[i], i);
+        }
+        return new int[]{0, 1};
+    }
+}
